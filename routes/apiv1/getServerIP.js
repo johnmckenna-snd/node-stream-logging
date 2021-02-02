ffc21a6d52d7cdb5a95d67ec02a8c1985302c672
johnmckenna-snd <ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDpa6B/irwN7xAiRc6pqo6GlShOsOGmFRBoQw1qoAlMv sndwrksdesign@gmail.com>
2021-02-01 22:26:10 -0800

    getSeverIP returns ipv4 address from en0

--- routes/apiv1/getServerIP.js
+++ routes/apiv1/getServerIP.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import os from 'os';
+
 const router = express.Router();
+const networkInterfaces = os.networkInterfaces();
+const en0ipv4 = networkInterfaces.en0[1];
 
 router.get('/get-server-ip', (req, res) => {
-	res.send('woot');
+	res.send({'serverIP': en0ipv4});
 });
 
 export { router as getServerIP };
